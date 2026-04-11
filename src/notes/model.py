26915d791e2b41bd5757943e1bab9e68d8ea9ca9
brenoviske <reisb6774@gmail.com>
2026-04-11 10:58:55 -0300

    feat:Introducing new columns to the note table model

--- src/notes/model.py
+++ src/notes/model.py
@@ -1,5 +1,6 @@
+from sqlalchemy.orm import relationship
 from src.database.config import Base
-from sqlalchemy import Column, Integer, String , DateTime
+from sqlalchemy import Column, Integer, String , DateTime , ForeignKey , Enum
 from datetime import datetime
 
 
@@ -10,7 +11,12 @@ class Note(Base):
     id = Column(Integer , primary_key = True , autoincrement = True  , nullable = False)
     title = Column(String(200) , nullable = False , index = True)
     content = Column(String(1024),  nullable = False , index = True)
+    status = Column(Enum('Completed ', 'Pending ', 'Canceled'), nullable = False , index = True)
+    priority = Column(Enum('High', 'Medium','Low'), nullable = False , index = True)
     created_at = Column(DateTime, nullable = False , index = True , default=datetime.now())
+
+    user_id = Column(Integer, ForeignKey('users.id') , nullable = False)
+    user = relationship('User', back_populates = 'notes')
 
     def to_dict(self):
 
